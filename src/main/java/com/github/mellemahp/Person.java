7ef1075aa47d69102eff5266582c6595def724db
juliomarcopineda <juliomarcopineda@gmail.com>
2020-07-19 15:20:32 -0700

    generate list of persons

--- src/main/java/com/github/mellemahp/Person.java
+++ src/main/java/com/github/mellemahp/Person.java
@@ -2,7 +2,7 @@ package com.github.mellemahp;
 
 import org.apache.commons.math3.distribution.RealDistribution;
 
-public abstract class Person {
+public class Person {
     private Person currentPartner;
     private PreferenceRanking preferenceRanking;
     private int preferenceIndex = 0;
@@ -15,15 +15,11 @@ public abstract class Person {
         this.preferenceRanking = new PreferenceRanking();
     }
 
-    public abstract void propose();
-
-    public abstract void reviewProposal();
-
     public int getPreferenceIndex() {
         return this.preferenceIndex;
     }
 
-    public <T extends Person> void initializePreferences(PersonList<T> personList) {
+    public <T extends Person> void initializePreferences(PersonList personList) {
         for (Person person : personList) {
             double firstImpressionScore = person.objectiveAttractivenessScore + this.preferenceDistribution.sample();
             this.preferenceRanking.add(
--- src/main/java/com/github/mellemahp/PersonList.java
+++ src/main/java/com/github/mellemahp/PersonList.java
@@ -6,33 +6,29 @@ import java.util.List;
 
 import org.apache.commons.math3.distribution.RealDistribution;
 
-public class PersonList<T extends Person> implements Iterable<T> {
-    private List<T> listOfPersons;
+public class PersonList implements Iterable<Person> {
+    private List<Person> listOfPersons;
 
     public PersonList(int numberOfPersons, RealDistribution objectiveAttractivenessDistribution, RealDistribution preferenceDistribution) {
         this.listOfPersons = this.generateListOfPersons(numberOfPersons, objectiveAttractivenessDistribution, preferenceDistribution);
     }
 
     
-    private List<T> generateListOfPersons(int numberOfPersons, RealDistribution objectiveAttractivenessDistribution, RealDistribution preferenceDistribution) {
-        List<T> listOfPersons = new ArrayList<>();
+    private List<Person> generateListOfPersons(int numberOfPersons, RealDistribution objectiveAttractivenessDistribution, RealDistribution preferenceDistribution) {
+        List<Person> listOfPersons = new ArrayList<>();
 
         for (int i = 0; i < numberOfPersons; i++) {
             double objectiveAttractivenessScore = objectiveAttractivenessDistribution.sample();
 
-            // TODO: This is currently not valid! We cannot create an instance of T. Furthermore,
-            // we cannot just construct and instance of type Person because Person is an abstract class
-            // Need to think about how to generate a Suitor/Suitee without specifying its type.
-            // listOfPersons.add(new T(objectiveAttractivenessScore, preferenceDistribution));
+            listOfPersons.add(new Person(objectiveAttractivenessScore, preferenceDistribution));
         }
 
         return listOfPersons;
     }
 
     @Override
-    public Iterator<T> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+    public Iterator<Person> iterator() {
+        return this.listOfPersons.iterator();
     }
     
 }
