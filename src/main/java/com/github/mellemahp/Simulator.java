72980c53419ee0b5be41012bca0521fd05aedc97
juliomarcopineda <juliomarcopineda@gmail.com>
2020-07-22 14:45:03 -0700

    Simulator constructor

--- src/main/java/com/github/mellemahp/Simulator.java
+++ src/main/java/com/github/mellemahp/Simulator.java
@@ -1,13 +1,42 @@
 package com.github.mellemahp;
 
-import java.util.List;
+import com.github.mellemahp.distribution.DistributionBuilder;
+
+import org.apache.commons.math3.distribution.RealDistribution;
 
 public class Simulator {
-    private List<Suitor> suitors;
-    private List<Suitee> suitees;
+    private PersonList suitors;
+    private PersonList suitees;
 
     public Simulator(SimulationConfig simulationConfig) {
+        // Make distributions for Suitor, Suitee and Preference
+        DistributionBuilder distributionBuilder = new DistributionBuilder();
         
+        RealDistribution suitorDistribution = distributionBuilder
+            .with(simulationConfig.getSuitorDistribution())
+            .build();
+        RealDistribution suiteeDistribution = distributionBuilder
+            .with(simulationConfig.getSuiteeDistribution())
+            .build();
+        RealDistribution preferenceDistribution = distributionBuilder
+            .with(simulationConfig.getPreferenceDistribution())
+            .build();
+        
+        // Build list of suitors and suitees
+        this.suitors = new PersonList(
+            simulationConfig.getNumberOfSuitors(),
+            suitorDistribution,
+            preferenceDistribution
+        );
+        this.suitees = new PersonList(
+            simulationConfig.getNumberOfSuitees(),
+            suiteeDistribution,
+            preferenceDistribution
+        );
+
+        // Initialize preference rankings for suitors and suitees
+        this.suitors.initializePreferenceList(this.suitees);
+        this.suitees.initializePreferenceList(this.suitors);
     }
     
 }
