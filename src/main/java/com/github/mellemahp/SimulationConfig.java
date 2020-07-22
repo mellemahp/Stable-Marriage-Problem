d290274a92c220461aba7cdcdaffc784be7384b0
juliomarcopineda <juliomarcopineda@gmail.com>
2020-07-22 14:04:09 -0700

    start simulation classes

---
+++ src/main/java/com/github/mellemahp/SimulationConfig.java
@@ -0,0 +1,29 @@
+package com.github.mellemahp;
+
+public class SimulationConfig {
+    private int numberOfSuitors;
+    private int numberOfSuitees;
+    private DistributionSettings suitorDistribution;
+    private DistributionSettings suiteeDistribution;
+    private DistributionSettings preferenceDistribution;
+
+    public int getNumberOfSuitors() {
+        return numberOfSuitors;
+    }
+
+    public int getNumberOfSuitees() {
+        return numberOfSuitees;
+    }
+
+    public DistributionSettings getSuitorDistribution() {
+        return suitorDistribution;
+    }
+
+    public DistributionSettings getSuiteeDistribution() {
+        return suiteeDistribution;
+    }
+
+    public DistributionSettings getPreferenceDistribution() {
+        return preferenceDistribution;
+    }
+}
---
+++ src/main/java/com/github/mellemahp/Simulator.java
@@ -0,0 +1,13 @@
+package com.github.mellemahp;
+
+import java.util.List;
+
+public class Simulator {
+    private List<Suitor> suitors;
+    private List<Suitee> suitees;
+
+    public Simulator(SimulationConfig simulationConfig) {
+        
+    }
+    
+}
