9354f9c7753d97186539cb4ea1523b4544081037
Hunter Mellema <mellemahp@gmail.com>
2020-07-22 19:55:59 -0600

    Add first version of config file parsing

--- src/main/java/com/github/mellemahp/simulation/SimulationConfig.java
+++ src/main/java/com/github/mellemahp/simulation/SimulationConfig.java
@@ -1,6 +1,15 @@
 package com.github.mellemahp.simulation;
 
+import java.io.InputStream;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidParameterException;
+import java.util.Map;
+
+import com.github.mellemahp.distribution.BetaDistributionSettings;
 import com.github.mellemahp.distribution.DistributionSettings;
+import com.github.mellemahp.distribution.NormalDistributionSettings;
+
+import org.yaml.snakeyaml.Yaml;
 
 public class SimulationConfig {
     private int numberOfSuitors;
@@ -9,8 +18,34 @@ public class SimulationConfig {
     private DistributionSettings suiteeDistribution;
     private DistributionSettings preferenceDistribution;
 
-    public SimulationConfig() {
-        // TODO: Handle reading config file
+    public SimulationConfig(InputStream configFileStream) {
+        Yaml yaml = new Yaml();
+        Map<String, Map<String, Object>> configs = yaml.load(configFileStream);
+        Map<String, Object> suiteeConfigs = configs.get("SuiteeConfigs"); 
+        Map<String, Object> suitorConfigs = configs.get("SuiteeConfigs"); 
+        Map<String, Object> preferenceConfigs = configs.get("PreferenceConfigs");
+        
+        this.numberOfSuitors = (int) suitorConfigs.get("numberOfSuitors");
+        this.numberOfSuitees = (int) suiteeConfigs.get("numberOfSuitees");
+        this.suiteeDistribution = createDistributionSettingsFromConfig((Map<String, Object>) suiteeConfigs.get("suiteeDistribution"));
+        this.preferenceDistribution = createDistributionSettingsFromConfig((Map<String, Object>) suitorConfigs.get("suitorDistribution"));
+        this.suitorDistribution = createDistributionSettingsFromConfig((Map<String, Object>) preferenceConfigs.get("preferenceDistribution"));
+    }
+
+    private DistributionSettings createDistributionSettingsFromConfig(Map<String, Object> distributionConfig) throws InvalidParameterException { 
+        String distType = (String) distributionConfig.get("DistributionType"); 
+        switch(distType){
+            case "Normal":
+                return new NormalDistributionSettings(
+                    (double) distributionConfig.get("mean"), 
+                    (double) distributionConfig.get("standardDeviation"));
+            case "Beta":
+                return new BetaDistributionSettings(
+                    (double) distributionConfig.get("alpha"), 
+                    (double) distributionConfig.get("beta"));
+            default: 
+                throw new InvalidParameterException(String.format("Distribution type {} not valid", distType));
+        }
     }
 
     public int getNumberOfSuitors() {
--- src/main/java/com/github/mellemahp/simulation/SimulationRunner.java
+++ src/main/java/com/github/mellemahp/simulation/SimulationRunner.java
@@ -1,9 +1,14 @@
 package com.github.mellemahp.simulation;
 
-public class SimulationRunner {
-    public static void main(String[] args) {
-        SimulationConfig simulationConfig = new SimulationConfig();
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
+public class SimulationRunner {
+    public static void main(String[] args) throws FileNotFoundException {
+        InputStream config = new FileInputStream(new File(args[0]));
+        SimulationConfig simulationConfig = new SimulationConfig(config);
         Simulator simulator = new Simulator(simulationConfig);
         simulator.run();
         simulator.printResults();
