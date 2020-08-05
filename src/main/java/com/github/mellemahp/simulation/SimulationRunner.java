8ecba58ecdcda8dae2ccae8b1803c347cf3a9c1e
Hunter Mellema <mellemahp@gmail.com>
2020-08-04 20:30:27 -0600

    Move parallel stream over to use a fork-join pool

--- src/main/java/com/github/mellemahp/simulation/SimulationRunner.java
+++ src/main/java/com/github/mellemahp/simulation/SimulationRunner.java
@@ -2,30 +2,46 @@ package com.github.mellemahp.simulation;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 
 import lombok.CustomLog;
 
 @CustomLog
 public class SimulationRunner {
-    public static List<Integer> runSimulations(List<Simulator> sims) {
-        return sims.parallelStream()
-            .map(Simulator::run)
-            .collect(Collectors.toUnmodifiableList());
+    public static void runSimulations(List<Simulator> sims) {
+        // Runs simulations in parallel
+        final int parallelism = 4;
+        ForkJoinPool forkJoinPool = null;
+        try {
+            forkJoinPool = new ForkJoinPool(parallelism);
+            forkJoinPool.submit(
+                    () -> sims.parallelStream()
+                            .map(Simulator::run)
+                            .collect(Collectors.toList()))
+                    .get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (forkJoinPool != null) {
+                forkJoinPool.shutdown();
+            }
+        }
     }
 
     public static void main(String[] args) {
 
         log.info("Simulation Runner Starting");
         List<String> files = Arrays.asList(args);
-        
+
         // Concurrently loads simulation files
         log.info("Loading Configuration files...");
         List<Simulator> sims = SimulationLoader.loadSimulations(files);
         log.info(sims.size() + " simulations found. Starting...");
 
-        // Runs simulations in parallel
-        List<Integer> _output = runSimulations(sims);
+        // Run simulations in parallel
+        runSimulations(sims);
         log.info("Run complete.");
     }
 }
