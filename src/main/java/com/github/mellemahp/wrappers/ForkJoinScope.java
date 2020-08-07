36a5822e71b52044607e24d5f55c86bb6d2ddc71
Hunter Mellema <mellemahp@gmail.com>
2020-08-06 21:41:48 -0600

    Refactoring parallel execution

--- src/main/java/com/github/mellemahp/wrappers/ForkJoinScope.java
+++ src/main/java/com/github/mellemahp/wrappers/ForkJoinScope.java
@@ -5,40 +5,32 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 
 import lombok.CustomLog;
 
 @CustomLog
 public class ForkJoinScope<T> {
-    private final int parallelism;
     private final ForkJoinPool forkJoinPool;
-    private final List<Callable<T>> taskList = new ArrayList<>();
+    private final List<Callable<T>> forkJoinTasks = new ArrayList<>();
 
     public ForkJoinScope(int parallelismLevel) {
-        parallelism = parallelismLevel;
         forkJoinPool = new ForkJoinPool(parallelismLevel);
     }
 
     public void addTask(Callable<T> task) { 
-        taskList.add(task);
+        forkJoinTasks.add(task);
     }
 
-    public void clear() {
-        taskList.clear();
+    public void clear() { 
+        forkJoinTasks.clear();
     }
 
     public void executeTasks() { 
         try {
-            for (Callable<T> task: taskList) {
-                this.forkJoinPool.submit(task).get();
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            log.warning("Error encountered while executing simulations in parallel");
-            throw new RuntimeException(e);
+            forkJoinPool.invokeAll(forkJoinTasks);
         } finally {
-            if (forkJoinPool != null) {
-                forkJoinPool.shutdown();
-            }
+            forkJoinPool.shutdown();
         }
     }
 }
