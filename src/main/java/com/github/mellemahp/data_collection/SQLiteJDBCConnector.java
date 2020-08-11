d8d5e474d95a803b19e0fa3d0dd5b00920e9f001
Hunter Mellema <mellemahp@gmail.com>
2020-08-10 21:26:45 -0600

    Convert SQL classes to use try-with-resources

--- src/main/java/com/github/mellemahp/data_collection/SQLiteJDBCConnector.java
+++ src/main/java/com/github/mellemahp/data_collection/SQLiteJDBCConnector.java
@@ -3,37 +3,15 @@ package com.github.mellemahp.data_collection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import lombok.CustomLog;
-import lombok.NonNull;
+
 
 @CustomLog
 public class SQLiteJDBCConnector {
    private static final String CLASS_LOADER = "org.sqlite.JDBC";
    private final String connectionString;
-
-   private void connectionContext(SQLStatementExecutor sqlToExecute) { 
-      Connection connection = null;
-      try {
-         connection = DriverManager.getConnection(connectionString);
-         SQLStatementContext statementContext = new SQLStatementContext(connection);
-         statementContext.runInScopedContext(sqlToExecute);
-      } catch (SQLException e) { 
-         log.severe("Unable to connect to database " + connectionString + " " + e.getMessage());
-      } finally { 
-         closeDBConnection(connection);
-      }
-   }
-
-   private void closeDBConnection(Connection connection) {
-      if (connection != null) {
-         try { 
-            connection.close();
-         } catch(SQLException e) { 
-            log.severe("Error closing db connection " + e.getMessage());
-         }
-      }
-   }
 
    private void loadSQLDriver() { 
       try { 
@@ -44,7 +22,16 @@ public class SQLiteJDBCConnector {
    }
 
    public SQLiteJDBCConnector(String dataBasePath) { 
-         loadSQLDriver();
-         connectionString = String.format("jdbc:sqlite:%s", dataBasePath);
+      loadSQLDriver();
+      connectionString = String.format("jdbc:sqlite:%s", dataBasePath);
+   }
+   
+   public void executeInConnectionContext(SQLStatementExecutor sqlExecutor) { 
+      try (Connection connection = DriverManager.getConnection(connectionString); 
+           Statement statement = connection.createStatement()){
+         sqlExecutor.execute(statement);
+      } catch (SQLException e) { 
+         log.severe("Unable to connect to database " + connectionString + " " + e.getMessage());
+      }
    }
 }
